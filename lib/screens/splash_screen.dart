a9b2de728ba22e27252311454f6b6e25c39e33c6
abhishekbhonde51 <abhishekbhonde567@gmail.com>
2024-04-17 16:37:48 +0530

    added register screen and splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,39 +1,77 @@
+import 'package:expensemanager/screens/register_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main() => runApp(const SplashScreen());
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
-  @override
+
   State createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              GestureDetector(
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Spacer(),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const RegisterScreen(),
+                  ),
+                );
+              },
+              child: Align(
+                alignment: Alignment.center,
                 child: Container(
                   height: 144,
                   width: 144,
-                  decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(100)),
-                      color: Color.fromRGBO(234, 238, 235, 1)),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                    color: const Color.fromRGBO(234, 238, 235, 1),
+                  ),
                   child: Center(
-                      child: Image.asset(
-                    'Assets/splash.png',
-                    width: 69,
-                    height: 59,
-                  )),
+                    child: Image.asset(
+                      'Assets/splash.png',
+                      width: 69,
+                      height: 59,
+                    ),
+                  ),
                 ),
-              )
-            ],
-          ),
+              ),
+            ),
+            const Spacer(),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 50),
+              child: Text(
+                "Expense Manager",
+                style: GoogleFonts.poppins(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 16,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
