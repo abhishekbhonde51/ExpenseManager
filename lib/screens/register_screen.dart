fcb07b7c23fc14d97ac3b66711b4e63ecee3e642
abhishekbhonde51 <abhishekbhonde_cs@jspmrscoe.edu.in>
2024-04-17 17:13:48 +0530

    completed registration screen

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:expensemanager/screens/signin_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -15,190 +16,199 @@ class _RegisterScreenState extends State {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 86,
-            ),
-            Image.asset(
-              "Assets/splash.png",
-              width: 69.76,
-              height: 58.82,
-            ),
-            const SizedBox(
-              height: 25,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(40),
-              child: Container(
-                child: Column(
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.only(right: 110),
-                      child: Text("Create your Account",
-                          style: GoogleFonts.poppins(
-                              fontWeight: FontWeight.w500,
-                              fontSize: 16,
-                              color: Colors.black)),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Container(
-                      height: 49,
-                      width: 280,
-                      decoration: const BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(8)),
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                                offset: Offset(0, 3),
-                                blurRadius: 10,
-                                color: Color.fromRGBO(0, 0, 0, 0.15))
-                          ]),
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                            border: InputBorder.none,
-                            labelText: "Name",
-                            labelStyle: GoogleFonts.poppins(
-                                fontWeight: FontWeight.w400,
-                                fontSize: 12,
-                                color: const Color.fromRGBO(0, 0, 0, 0.4)),
-                            contentPadding: EdgeInsets.only(left: 30)),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 86,
+              ),
+              Image.asset(
+                "Assets/splash.png",
+                width: 69.76,
+                height: 58.82,
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(40),
+                child: Container(
+                  child: Column(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(right: 110),
+                        child: Text("Create your Account",
+                            style: GoogleFonts.poppins(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 16,
+                                color: Colors.black)),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Container(
-                      height: 49,
-                      width: 280,
-                      decoration: const BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(8)),
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                                offset: Offset(0, 3),
-                                blurRadius: 10,
-                                color: Color.fromRGBO(0, 0, 0, 0.15))
-                          ]),
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                            border: InputBorder.none,
-                            labelText: "Username",
-                            labelStyle: GoogleFonts.poppins(
-                                fontWeight: FontWeight.w400,
-                                fontSize: 12,
-                                color: const Color.fromRGBO(0, 0, 0, 0.4)),
-                            contentPadding: EdgeInsets.only(left: 30)),
+                      const SizedBox(
+                        height: 20,
                       ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Container(
-                      height: 49,
-                      width: 280,
-                      decoration: const BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(8)),
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                                offset: Offset(0, 3),
-                                blurRadius: 10,
-                                color: Color.fromRGBO(0, 0, 0, 0.15))
-                          ]),
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                            border: InputBorder.none,
-                            labelText: "Password",
-                            labelStyle: GoogleFonts.poppins(
-                                fontWeight: FontWeight.w400,
-                                fontSize: 12,
-                                color: const Color.fromRGBO(0, 0, 0, 0.4)),
-                            contentPadding: EdgeInsets.only(left: 30)),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Container(
-                      height: 49,
-                      width: 280,
-                      decoration: const BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(8)),
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                                offset: Offset(0, 3),
-                                blurRadius: 10,
-                                color: Color.fromRGBO(0, 0, 0, 0.15))
-                          ]),
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                            border: InputBorder.none,
-                            labelText: "Confirm Password",
-                            labelStyle: GoogleFonts.poppins(
-                                fontWeight: FontWeight.w400,
-                                fontSize: 12,
-                                color: const Color.fromRGBO(0, 0, 0, 0.4)),
-                            contentPadding: const EdgeInsets.only(left: 30)),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Container(
-                      height: 49,
-                      width: 280,
-                      decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(8)),
-                        color: Color.fromRGBO(14, 161, 125, 1),
-                      ),
-                      child: Center(
-                        child: Text(
-                          "Sign Up",
-                          style: GoogleFonts.poppins(
-                              fontSize: 15,
-                              fontWeight: FontWeight.w500,
-                              color: Colors.white),
+                      Container(
+                        height: 49,
+                        width: 280,
+                        decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(8)),
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                  offset: Offset(0, 3),
+                                  blurRadius: 10,
+                                  color: Color.fromRGBO(0, 0, 0, 0.15))
+                            ]),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              labelText: "Name",
+                              labelStyle: GoogleFonts.poppins(
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 12,
+                                  color: const Color.fromRGBO(0, 0, 0, 0.4)),
+                              contentPadding: const EdgeInsets.only(left: 30)),
                         ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 200,
-                    ),
-                    Row(
-                      children: [
-                        Container(
-                          margin: const EdgeInsets.only(left: 40),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        height: 49,
+                        width: 280,
+                        decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(8)),
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                  offset: Offset(0, 3),
+                                  blurRadius: 10,
+                                  color: Color.fromRGBO(0, 0, 0, 0.15))
+                            ]),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              labelText: "Username",
+                              labelStyle: GoogleFonts.poppins(
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 12,
+                                  color: const Color.fromRGBO(0, 0, 0, 0.4)),
+                              contentPadding: EdgeInsets.only(left: 30)),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        height: 49,
+                        width: 280,
+                        decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(8)),
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                  offset: Offset(0, 3),
+                                  blurRadius: 10,
+                                  color: Color.fromRGBO(0, 0, 0, 0.15))
+                            ]),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              labelText: "Password",
+                              labelStyle: GoogleFonts.poppins(
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 12,
+                                  color: const Color.fromRGBO(0, 0, 0, 0.4)),
+                              contentPadding: EdgeInsets.only(left: 30)),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        height: 49,
+                        width: 280,
+                        decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(8)),
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                  offset: Offset(0, 3),
+                                  blurRadius: 10,
+                                  color: Color.fromRGBO(0, 0, 0, 0.15))
+                            ]),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              labelText: "Confirm Password",
+                              labelStyle: GoogleFonts.poppins(
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 12,
+                                  color: const Color.fromRGBO(0, 0, 0, 0.4)),
+                              contentPadding: const EdgeInsets.only(left: 30)),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        height: 49,
+                        width: 280,
+                        decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(8)),
+                          color: Color.fromRGBO(14, 161, 125, 1),
+                        ),
+                        child: Center(
                           child: Text(
-                            "Already have an account?",
+                            "Sign Up",
                             style: GoogleFonts.poppins(
-                                fontWeight: FontWeight.w400,
-                                color: const Color.fromRGBO(0, 0, 0, 0.6)),
+                                fontSize: 15,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.white),
                           ),
                         ),
-                        const SizedBox(
-                          width: 2,
-                        ),
-                        GestureDetector(
-                          child: Text("Sign in",
+                      ),
+                      const SizedBox(
+                        height: 200,
+                      ),
+                      Row(
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.only(left: 40),
+                            child: Text(
+                              "Already have an account?",
                               style: GoogleFonts.poppins(
                                   fontWeight: FontWeight.w400,
-                                  color:
-                                      const Color.fromRGBO(14, 161, 125, 1))),
-                        ),
+                                  color: const Color.fromRGBO(0, 0, 0, 0.6)),
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 2,
+                          ),
+                          GestureDetector(
+                            onTap: () => {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const SigninScreen(),
+                                  ))
+                            },
+                            child: Text("Sign in",
+                                style: GoogleFonts.poppins(
+                                    fontWeight: FontWeight.w400,
+                                    color:
+                                        const Color.fromRGBO(14, 161, 125, 1))),
+                          ),
 
-                        // const Spacer()
-                      ],
-                    )
-                  ],
+                          // const Spacer()
+                        ],
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
