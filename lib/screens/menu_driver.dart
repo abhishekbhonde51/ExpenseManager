4e90febe86dcc2488f4a91bea286ccb7139a1817
abhishekbhonde51 <abhishekbhonde_cs@jspmrscoe.edu.in>
2024-04-18 20:56:37 +0530

    added menu drawer

---
+++ lib/screens/menu_driver.dart
@@ -0,0 +1,216 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(
+    const MenuDriver(),
+  );
+}
+
+class MenuDriver extends StatelessWidget {
+  const MenuDriver({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(),
+        drawer: Drawer(
+          child: Row(
+            children: [
+              Column(
+                children: [
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(left: 25),
+                    child: Text(
+                      "Expense Manager",
+                      style: GoogleFonts.poppins(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(left: 25),
+                    child: Text(
+                      "Saves all your Transactions",
+                      style: GoogleFonts.poppins(
+                          fontSize: 10,
+                          fontWeight: FontWeight.w400,
+                          color: const Color.fromRGBO(0, 0, 0, 0.5)),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    height: 40,
+                    width: 184,
+                    decoration: const BoxDecoration(
+                      color: Color.fromRGBO(14, 161, 125, 0.15),
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(20),
+                          bottomRight: Radius.circular(20)),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: Row(
+                        children: [
+                          const Icon(Icons.list),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          Text(
+                            "Transaction",
+                            style: GoogleFonts.poppins(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 16,
+                                color: const Color.fromRGBO(14, 161, 125, 1)),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    height: 40,
+                    width: 184,
+                    decoration: const BoxDecoration(
+                      // color: Color.fromRGBO(14, 161, 125, 0.15),
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(20),
+                          bottomRight: Radius.circular(20)),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: Row(
+                        children: [
+                          const Icon(Icons.graphic_eq),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          Text(
+                            "Graph",
+                            style: GoogleFonts.poppins(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 16,
+                              // color: const Color.fromRGBO(14, 161, 125, 1)
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    height: 40,
+                    width: 184,
+                    decoration: const BoxDecoration(
+                      // color: Color.fromRGBO(14, 161, 125, 0.15),
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(20),
+                          bottomRight: Radius.circular(20)),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: Row(
+                        children: [
+                          const Icon(Icons.category),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          Text(
+                            "Category",
+                            style: GoogleFonts.poppins(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 16,
+                              // color: const Color.fromRGBO(14, 161, 125, 1)
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    height: 40,
+                    width: 184,
+                    decoration: const BoxDecoration(
+                      // color: Color.fromRGBO(14, 161, 125, 0.15),
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(20),
+                          bottomRight: Radius.circular(20)),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: Row(
+                        children: [
+                          const Icon(Icons.delete),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          Text(
+                            "Trash",
+                            style: GoogleFonts.poppins(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 16,
+                              // color: const Color.fromRGBO(14, 161, 125, 1)
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    height: 40,
+                    width: 184,
+                    decoration: const BoxDecoration(
+                      // color: Color.fromRGBO(14, 161, 125, 0.15),
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(20),
+                          bottomRight: Radius.circular(20)),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: Row(
+                        children: [
+                          const Icon(Icons.person),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          Text(
+                            "About us",
+                            style: GoogleFonts.poppins(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 16,
+                              // color: const Color.fromRGBO(14, 161, 125, 1)
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  )
+                ],
+              ),
+            ],
+          ),
+        ));
+  }
+}
